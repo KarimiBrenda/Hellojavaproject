3cc0c3c94fd49dab401d6626e9c545d6b0b91e13
KarimiBrenda <kbuuri83@gmail.com>
2024-02-22 15:47:09 +0300

    Added a comments.java file

---
+++ comments.java
@@ -0,0 +1,22 @@
+public class comments {
+    public static void main(String[] args){
+        /* Comments in Java
+        * We have Three types of comments in java
+        * 1. c++ Style comments --> //
+        * 2. C Style comments
+        * 3. special javadoc comments */
+
+        //1. C++ comments Style
+     System.out.println("We use // for C++ comment style");
+
+        /* 2. C comment Style */
+        System.out.println("We use /** for C comment style");
+
+        /**
+         * 3. Special javadoc comment
+         */
+        System.out.println("3. We use /** */ for special javadoc comment");
+
+
+    }
+}
